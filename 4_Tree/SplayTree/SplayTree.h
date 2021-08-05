40f307281b7a7f9f100a3ce9bd80b38d8adb3e10
Junjie_Li <jfjunjieli@gmail.com>
2021-08-05 18:49:48 +0200

    add splay tree

---
+++ 4_Tree/SplayTree/SplayTree.h
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   SplayTree.h
+ * Author: Junjie_Li
+ *
+ * Created on August 5, 2021, 6:46 PM
+ */
+
+#ifndef SPLAYTREE_H
+#define SPLAYTREE_H
+
+class SplayTree {
+public:
+    SplayTree();
+    SplayTree(const SplayTree& orig);
+    virtual ~SplayTree();
+private:
+
+};
+
+#endif /* SPLAYTREE_H */
+
